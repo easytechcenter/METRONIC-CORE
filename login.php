9db5008e98f94d7b9b004197355b7e60686c3d2a
easytechcenter <m.maxime@easytechcenter.com>
2015-01-26 00:35:32 +0100

    COMMIT 26115-1C
    1C

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ $donnee_version = mysql_fetch_array($sql_version);
         <!-- END META SECTION -->
         
         <!-- CSS INCLUDE -->        
-        <link rel="stylesheet" type="text/css" id="theme" href="<?php echo $rootsite; ?>/assets/css/theme-default.css"/>
+        <link rel="stylesheet" type="text/css" id="theme" href="<?php echo SITE_URL,RACINE,ASSETS; ?>css/theme-default.css"/>
         <!-- EOF CSS INCLUDE -->                                    
     </head>
     <body>
