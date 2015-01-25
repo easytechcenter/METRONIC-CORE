cca66a18be267e0a5d3ff16a4106c980663827c0
easytechcenter <m.maxime@easytechcenter.com>
2015-01-25 17:40:22 +0100

    COMMIT 25115-3

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,12 @@
 <?php
+//base
+$host = "localhost";
+$user = "root";
+$pass = "1992maxime";
+$base = "metronic";
 
+mysql_connect($host, $user, $pass)or die(mysql_error());
+mysql_select_db($base);
 
 
 $rootsite = "http://vps116895.ovh.net/metronic/";
