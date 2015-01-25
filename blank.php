6410bcda8c07a421bf27634fdaeaddd4cc57e56f
easytechcenter <m.maxime@easytechcenter.com>
2015-01-25 20:48:45 +0100

    COMMIT 25115-5(FINAL)
    BLOCAGE FICHIER PREPARER

--- blank.php
+++ blank.php
@@ -55,15 +55,15 @@ define("PAGE", ""); // Nom de la Page
         <div class="message-box animated fadeIn" data-sound="alert" id="mb-signout">
             <div class="mb-container">
                 <div class="mb-middle">
-                    <div class="mb-title"><span class="fa fa-sign-out"></span> Log <strong>Out</strong> ?</div>
+                    <div class="mb-title"><span class="fa fa-sign-out"></span> Voulez-vous vous <strong>Déconnecter</strong> ?</div>
                     <div class="mb-content">
-                        <p>Are you sure you want to log out?</p>                    
-                        <p>Press No if youwant to continue work. Press Yes to logout current user.</p>
+                        <p>Etes-vous sur de vouloir vous déconnecter du système ?</p>                    
+                        <p>Cliquez sur <strong>Non</strong> si vous voulez continuer votre travail.<br> Cliquez sur <strong>Oui</strong> si vous voulez vous déconnecter de l'interface.</p>
                     </div>
                     <div class="mb-footer">
                         <div class="pull-right">
-                            <a href="pages-login.html" class="btn btn-success btn-lg">Yes</a>
-                            <button class="btn btn-default btn-lg mb-control-close">No</button>
+                            <a href="<?php echo SITE_URL,RACINE; ?>logout.php" class="btn btn-success btn-lg">Oui</a>
+                            <button class="btn btn-default btn-lg mb-control-close">Non</button>
                         </div>
                     </div>
                 </div>
