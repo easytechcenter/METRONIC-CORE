ec499edea3e6ec4609c16e92481e2ec7dcb6d0a2
easytechcenter <m.maxime@easytechcenter.com>
2015-01-25 20:07:06 +0100

    COMMIT 25115-5k
    5k

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -25,59 +25,35 @@
                         </div>                                                                        
                     </li>
                     <li class="active">
-                        <a href="index.html"><span class="fa fa-desktop"></span> <span class="xn-text">Dashboard</span></a>                        
+                        <a href="<?php echo $rootsite; ?>"><span class="fa fa-desktop"></span> <span class="xn-text">Accueil</span></a>                        
                     </li>                    
                     <li class="xn-openable">
-                        <a href="#"><span class="fa fa-files-o"></span> <span class="xn-text">Pages</span></a>
+                        <a href="#">
+                            <span class="fa fa-cogs"></span> 
+                            <span class="xn-text">Paramétrage</span>
+                        </a>
                         <ul>
-                            <li><a href="pages-gallery.html"><span class="fa fa-image"></span> Gallery</a></li>
-                            <li><a href="pages-profile.html"><span class="fa fa-user"></span> Profile</a></li>
-                            <li><a href="pages-address-book.html"><span class="fa fa-users"></span> Address Book</a></li>
-                            <li class="xn-openable">
-                                <a href="#"><span class="fa fa-clock-o"></span> Timeline</a>
-                                <ul>
-                                    <li><a href="pages-timeline.html"><span class="fa fa-align-center"></span> Default</a></li>
-                                    <li><a href="pages-timeline-simple.html"><span class="fa fa-align-justify"></span> Full Width</a></li>
-                                </ul>
+                             <li>
+                                <a href="<?php echo $rootsite; ?>module/parametre/societe/">
+                                    <span class="fa fa-building-o"></span> Gestion de la société
+                                </a>
                             </li>
-                            <li class="xn-openable">
-                                <a href="#"><span class="fa fa-envelope"></span> Mailbox</a>
-                                <ul>
-                                    <li><a href="pages-mailbox-inbox.html"><span class="fa fa-inbox"></span> Inbox</a></li>
-                                    <li><a href="pages-mailbox-message.html"><span class="fa fa-file-text"></span> Message</a></li>
-                                    <li><a href="pages-mailbox-compose.html"><span class="fa fa-pencil"></span> Compose</a></li>
-                                </ul>
+                            <li>
+                                <a href="<?php echo $rootsite; ?>module/parametre/user/">
+                                    <span class="fa fa-users"></span> Gestion des Utilisateurs
+                                </a>
                             </li>
-                            <li><a href="pages-messages.html"><span class="fa fa-comments"></span> Messages</a></li>
-                            <li><a href="pages-calendar.html"><span class="fa fa-calendar"></span> Calendar</a></li>
-                            <li><a href="pages-tasks.html"><span class="fa fa-edit"></span> Tasks</a></li>
-                            <li><a href="pages-content-table.html"><span class="fa fa-columns"></span> Content Table</a></li>
-                            <li><a href="pages-faq.html"><span class="fa fa-question-circle"></span> FAQ</a></li>
-                            <li><a href="pages-search.html"><span class="fa fa-search"></span> Search</a></li>
-                            <li class="xn-openable">
-                                <a href="#"><span class="fa fa-file"></span> Blog</a>
-                                
-                                <ul>                                    
-                                    <li><a href="pages-blog-list.html"><span class="fa fa-copy"></span> List of Posts</a></li>
-                                    <li><a href="pages-blog-post.html"><span class="fa fa-file-o"></span>Single Post</a></li>
-                                </ul>
+                            <li>
+                                <a href="<?php echo $rootsite; ?>module/parametre/update/">
+                                    <span class="fa fa-download"></span> Mise à jour
+                                </a>
                             </li>
-                            <li class="xn-openable">
-                                <a href="#"><span class="fa fa-sign-in"></span> Login</a>
-                                <ul>                                    
-                                    <li><a href="pages-login.html">App Login</a></li>
-                                    <li><a href="pages-login-website.html">Website Login</a></li>
-                                    <li><a href="pages-login-website-light.html"> Website Login Light</a></li>
-                                </ul>
+                            <li>
+                                <a href="<?php echo $rootsite; ?>module/parametre/module/">
+                                    <span class="glyphicon glyphicon-th"></span> Gestion des modules
+                                </a>
                             </li>
-                            <li class="xn-openable">
-                                <a href="#"><span class="fa fa-warning"></span> Error Pages</a>
-                                <ul>                                    
-                                    <li><a href="pages-error-404.html">Error 404 Sample 1</a></li>
-                                    <li><a href="pages-error-404-2.html">Error 404 Sample 2</a></li>
-                                    <li><a href="pages-error-500.html"> Error 500</a></li>
-                                </ul>
-                            </li>                            
+
                         </ul>
                     </li>                                                         
                 </ul>
