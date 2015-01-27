ed8c9bbdaed7cb072690719495e2fa86f4784645
easytechcenter <m.maxime@easytechcenter.com>
2015-01-27 01:32:34 +0100

    COMMIT 27115-1Q

--- module/parametre/societe/index.php
+++ module/parametre/societe/index.php
@@ -28,6 +28,27 @@ define("PAGE", "Gestion de la société"); // Nom de la Page
                 <?php
                 $sql_societe = mysql_query("SELECT * FROM societe WHERE idsociete = '1'")or die(mysql_error());
                 $donnee_societe = mysql_fetch_array($sql_societe);
+                //Verif siret
+                function is_siret($siret)
+                    {
+                        if (strlen($siret) != 14) return 1; // le SIRET doit contenir 14 caractères
+                        if (!is_numeric($siret)) return 2; // le SIRET ne doit contenir que des chiffres
+
+                        // on prend chaque chiffre un par un
+                        // si son index (position dans la chaîne en commence à 0 au premier caractère) est pair
+                        // on double sa valeur et si cette dernière est supérieure à 9, on lui retranche 9
+                        // on ajoute cette valeur à la somme totale
+
+                        for ($index = 0; $index < 14; $index ++)
+                        {
+                            $number = (int) $siret[$index];
+                            if (($index % 2) == 0) { if (($number *= 2) > 9) $number -= 9; }
+                            $sum += $number;
+                        }
+
+                        // le numéro est valide si la somme des chiffres est multiple de 10
+                        if (($sum % 10) != 0) return 3; else return 0;      
+                    }
                 ?>
                 <!-- PAGE CONTENT WRAPPER -->
                 <div class="page-content-wrap">
@@ -127,6 +148,33 @@ define("PAGE", "Gestion de la société"); // Nom de la Page
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h3 class="panel-title">Information professionnel</h3>
+                                    </div>
+                                    <div class="panel-body">  
+                                        <?php
+                                        if(is_siret($donnee_societe['siret']) == 0){
+                                        ?>
+                                        <div class="form-group has-success has-feedback">
+                                            <label class="control-label">Siret</label>
+                                            <input type="text" class="form-control" name="siret" value="<?php echo $donnee_societe['siret']; ?>">
+                                            <span class="glyphicon glyphicon-ok form-control-feedback"></span>
+                                        </div>
+                                        <?php }else{ ?>
+                                        <div class="form-group has-error has-feedback">
+                                            <label class="control-label">Siret</label>
+                                            <input type="text" class="form-control">
+                                            <span class="glyphicon glyphicon-remove form-control-feedback"></span>
+                                        </div>
+                                        <?php } ?>
+                                        <br>
+                                        
                                     </div>
                                 </div>
                             </div>
