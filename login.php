5adc1fa765fe47bf8ff1b60e20f42aaca8a74be6
easytechcenter <m.maxime@easytechcenter.com>
2015-02-01 02:22:40 +0100

    COMMIT 1215-1H

--- login.php
+++ login.php
@@ -1,4 +1,56 @@
-<?php include ('config/header.php'); ?>
+<?php
+include ('config/db.conf.php');
+include ('config/define.php');
+?>
+<!DOCTYPE html>
+<!-- 
+TEMPLATE NAME : Adminre - backend
+VERSION : 1.3.0
+AUTHOR : JohnPozy
+AUTHOR URL : http://themeforest.net/user/JohnPozy
+EMAIL : pampersdry@gmail.com
+LAST UPDATE: 2015/01/05
+
+** A license must be purchased in order to legally use this template for your project **
+** PLEASE SUPPORT ME. YOUR SUPPORT ENSURE THE CONTINUITY OF THIS PROJECT **
+-->
+<html class="backend">
+    <!-- START Head -->
+    <head>
+        <!-- START META SECTION -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Adminre - backend</title>
+        <meta name="author" content="pampersdry.info">
+        <meta name="description" content="Adminre is a clean and flat backend and frontend theme build with twitter bootstrap">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo SITE_URL, RACINE, STYLE; ?>/images/touch/apple-touch-icon-144x144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo SITE_URL, RACINE, STYLE; ?>/images/touch/apple-touch-icon-114x114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo SITE_URL, RACINE, STYLE; ?>/images/touch/apple-touch-icon-72x72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="<?php echo SITE_URL, RACINE, STYLE; ?>/images/touch/apple-touch-icon-57x57-precomposed.png">
+        <link rel="shortcut icon" href="<?php echo SITE_URL, RACINE, STYLE; ?>/images/favicon.ico">
+        <!--/ END META SECTION -->
+
+        <!-- START STYLESHEETS -->
+        <!-- Plugins stylesheet : optional -->
+        <!--/ Plugins stylesheet : optional -->
+
+        <!-- Application stylesheet : mandatory -->
+        <link rel="stylesheet" href="<?php echo SITE_URL, RACINE, STYLE; ?>css/bootstrap.css">
+        <link rel="stylesheet" href="<?php echo SITE_URL, RACINE, STYLE; ?>css/layout.css">
+        <link rel="stylesheet" href="<?php echo SITE_URL, RACINE, STYLE; ?>css/uielement.css">
+        <!--/ Application stylesheet -->
+
+        <!-- Theme stylesheet : optional -->
+        <!--/ Theme stylesheet : optional -->
+
+        <!-- modernizr script -->
+        <script type="text/javascript" src="<?php echo SITE_URL, RACINE, STYLE; ?>plugins/modernizr/js/modernizr.js"></script>
+        <!--/ modernizr script -->
+        <!-- END STYLESHEETS -->
+    </head>
+    <!--/ END Head -->
 <?php
             // on teste si le visiteur a soumis le formulaire de connexion
             date_default_timezone_set("UTC");
