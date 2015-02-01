1dee3cf3a0c1dc6b024864a5237d52f27a0144e6
easytechcenter <m.maxime@easytechcenter.com>
2015-02-01 02:33:03 +0100

    COMMIT 1215-1O

--- login.php
+++ login.php
@@ -61,7 +61,7 @@ LAST UPDATE: 2015/01/05
                 if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass']))) {
 
                     // on teste si une entrée de la base contient ce couple login / pass
-                    $sql = 'SELECT count(*) FROM utilisateur WHERE login="'.mysql_escape_string($_POST['login']).'"  AND pass_md5="'.mysql_escape_string(md5($_POST['pass'])).'"';
+                    $sql = 'SELECT count(*) FROM utilisateur WHERE login="'.mysql_escape_string($_POST['login']).'"  AND pass="'.mysql_escape_string(md5($_POST['pass'])).'"';
                     $req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
                     $data = mysql_fetch_array($req);
                     mysql_free_result($req);
